7cd994ea64ded9f3cf4db9237b6339fa5db4acc8
Ilay <iliusha_1999nooh@mail.ru>
2020-09-17 10:05:17 +0300

    task-3

--- Module-2/task-3.js
+++ Module-2/task-3.js
@@ -1,18 +1,13 @@
 'use strict'
-let n = -3;
-let m = -10;
+let n = 100;
+let m = -5;
 
-if (n < 0 && m < 0){
-    let rN = Math.round(Math.random() * -Math.max(Math.abs(m) , Math.abs(n)))
-    if(rN % 2 != 0 && rN >= -Math.min(Math.abs(m) , Math.abs(n))) {
-        console.log(rN)
-    }
-}
-else{
-    let rN = Math.round(Math.random() * Math.max(m , n))
-    if(rN % 2 != 0 && rN >= Math.min(m , n)) {
-    console.log(rN)
-    }
-}
+let u = Math.max(n , m)
+let d = Math.min(n , m)
 
+let pN = Math.round(Math.random() * (u - d) + d)
+
+if(pN % 2 != 0 && pN >= d){
+    console.log(pN)
+}
 
