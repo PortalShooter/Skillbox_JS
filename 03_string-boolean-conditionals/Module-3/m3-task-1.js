1a8e8589a400f7e16ff90f992dabf48067d095d2
Ilay <iliusha_1999nooh@mail.ru>
2020-09-21 11:35:15 +0300

    fix

--- 03_string-boolean-conditionals/Module-3/m3-task-1.js
+++
@@ -1,7 +0,0 @@
-let password = '_-3';
-let n = password.length
-
-if(n >= 4 && (password.includes('-') || password.includes('_'))){
-    console.log('Пароль надежный')
-} else (console.log('Пароль недостаточно надёжный'))
-
