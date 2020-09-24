f427899409a06103cdd3cf2a907d24ccac9c114a
Ilay <iliusha_1999nooh@mail.ru>
2020-09-24 15:27:26 +0300

    correction

--- 04_arrays-and-loops/Module-4/m4-task-3.js
+++ 04_arrays-and-loops/Module-4/m4-task-3.js
@@ -1,20 +1,18 @@
 'use strict'
-let roadMines = [true, true, true, true, true, true, true, true, true, true]
+let roadMines = [true, false, false, false, false, false, false, false, false, true]
 
-let position
-let n = 0
+let position;
+let n = 0;
 step: for(position in roadMines){
-	console.log(`Танк переместился на ${+position + 1}`)
+	console.log(`Танк переместился на ${+position + 1}`);
 
-	for(roadMines[position]; roadMines[position] == true;){
-		if(n <= 1){
-			n += 1
-			console.log('Танк поврежден')
-			continue step;
-		}	else if(n == 2){
-			console.log('Танк уничтожен')
-			break step;
-		}
+	if(roadMines[position] === false && n <= 1){
+		n += 1;
+		console.log('Танк поврежден');
+		continue step;
+	} else if(roadMines[position] === false && n ===2){
+		console.log('Танк уничтожен');
+		break step;
 	}
 }
 
