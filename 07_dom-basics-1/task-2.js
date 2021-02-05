b00870473c40305b64948bf74f63631b8d6e15a7
PortalShooter <70528020+PortalShooter@users.noreply.github.com>
2021-02-05 10:16:51 +0300

    module 7

--- 07_dom-basics-1/task-2.js
+++ 07_dom-basics-1/task-2.js
@@ -1,27 +1,19 @@
 'use strict'
 let input = document.createElement('input');
 let h2 = document.createElement('h2');
-
 let task2 = document.querySelector('.task-2')
-
+let timeId
 task2.append(input)
 task2.append(h2)
 
-let timeout = setTimeout(assign, 5000)
+input.onkeydown = textOutput
 
 function textOutput() {
-  let momentValue = input.value
-
-  setInterval(check, 1000)
-  function check() {
-    if(momentValue != input.value){
-      momentValue = input.value;
-      clearTimeout(timeout.timeoutID)
-    }
+  clearTimeout(timeId)
+  console.log(timeId)
+  timeId = setTimeout(addContent, 300)
+  function addContent() {
+    h2.textContent = input.value;
   }
 }
-function assign() {
-  h2.textContent = input.value;
-  console.log('123')
-}
-textOutput()
+
