192777148f2cbaf3d857c203dc9c77fc8fb82805
Ilay Kutsevalov <iliusha_1999nooh@mail.ru>
2021-08-29 15:06:35 +0300

    вроде все готово

--- 10_type-transform/index.js
+++ 10_type-transform/index.js
@@ -41,9 +41,10 @@ let students = [
     faculty: 'Энергетика',
   },
 ];
+const table = document.createElement('table')
 
 function createTable(data) {
-  const table = document.createElement('table')
+
   table.classList.add('table')
   document.querySelector('.table').append(table)
   createTitle()
@@ -59,51 +60,49 @@ function createTable(data) {
     title.classList.add('title')
     table.append(title)
   }
-
-  function createRow(student) {
-    const row = document.createElement('tr')
-    row.classList.add('row')
-    table.append(row)
-    createColumn(row, student)
-
-    function createColumn(row, student) {
-      for (let i = 0; i < 4; i++) {
-        let column
-        const header = ['ФИО студента', 'Факультет', 'Дата рождения', 'Годы обучения']
-        if (student === 'search') {
-          column = document.createElement('th')
-          const inputSearch = document.createElement('input')
-          inputSearch.classList.add('input-search')
-          column.append(inputSearch)
-        } else if (student === 'header') {
-          column = document.createElement('th')
-          column.classList.add('header-column')
-          column.textContent = header[i]
-        }
-        else {
-          let startYears = new Date().getFullYear() - student.dateOfLearning
-          if (startYears > 4 || startYears == 4 && new Date().getMonth() > 8) startYears = 'Закончил'
-          column = document.createElement('td')
-          if (i === 0) column.textContent = `${student.surname} ${student.name} ${student.patronymic}`
-          else if (i === 1) column.textContent = `${student.faculty}`
-          else if (i === 2) column.textContent = `${student.dob.getDate()}.${student.dob.getMonth() + 1}.${student.dob.getFullYear()} (${new Date().getFullYear() - student.dob.getFullYear()})`
-          else if (i === 3) column.textContent = `${student.dateOfLearning}-${student.dateOfLearning + 4} (${startYears})`
-        }
-        row.append(column)
-      }
-    }
-  }
   sortTable()
 }
 createTable(students)
+function createRow(student) {
+  const row = document.createElement('tr')
+  row.classList.add('row')
+  table.append(row)
+  createColumn(row, student)
 
+  function createColumn(row, student) {
+    for (let i = 0; i < 4; i++) {
+      let column
+      const header = ['ФИО студента', 'Факультет', 'Дата рождения', 'Годы обучения']
+      if (student === 'search') {
+        column = document.createElement('th')
+        const inputSearch = document.createElement('input')
+        inputSearch.classList.add('input-search')
+        column.append(inputSearch)
+      } else if (student === 'header') {
+        column = document.createElement('th')
+        column.classList.add('header-column')
+        column.textContent = header[i]
+      }
+      else {
+        let startYears = new Date().getFullYear() - student.dateOfLearning
+        if (startYears > 4 || startYears == 4 && new Date().getMonth() > 8) startYears = 'Закончил'
+        column = document.createElement('td')
+        if (i === 0) column.textContent = `${student.surname} ${student.name} ${student.patronymic}`
+        else if (i === 1) column.textContent = `${student.faculty}`
+        else if (i === 2) column.textContent = `${student.dob.getDate()}.${student.dob.getMonth() + 1}.${student.dob.getFullYear()} (${new Date().getFullYear() - student.dob.getFullYear()})`
+        else if (i === 3) column.textContent = `${student.dateOfLearning}-${student.dateOfLearning + 4} (${startYears})`
+      }
+      row.append(column)
+    }
+  }
+}
 function sortTable() {
   const headerColumn = document.querySelectorAll('.header-column')
   headerColumn.forEach((cell, indexCell) => {
     cell.addEventListener('click', () => {
       if (indexCell === 0) {
         students.sort(function (a, b) {
-          let nameA = (a.surname + a.name + a.patronymic).toLowerCase(),
+          const nameA = (a.surname + a.name + a.patronymic).toLowerCase(),
             nameB = (b.surname + b.name + b.patronymic).toLowerCase()
           if (nameA < nameB) //сортируем строки по возрастанию
             return -1
@@ -114,7 +113,7 @@ function sortTable() {
       }
       if (indexCell === 1) {
         students.sort(function (a, b) {
-          let nameA = a.faculty.toLowerCase(),
+          const nameA = a.faculty.toLowerCase(),
             nameB = b.faculty.toLowerCase()
           if (nameA < nameB) //сортируем строки по возрастанию
             return -1
@@ -125,24 +124,18 @@ function sortTable() {
       }
       if (indexCell === 2) {
         students.sort(function (a, b) {
-          var dateA = new Date(a.dob), dateB = new Date(b.dob)
+          const dateA = new Date(a.dob), dateB = new Date(b.dob)
           return dateA - dateB //сортировка по возрастающей дате
         })
       }
       if (indexCell === 3) {
         students.sort(function (a, b) {
-          var dateA = new Date(a.dateOfLearning), dateB = new Date(b.dateOfLearning)
+          const dateA = new Date(a.dateOfLearning), dateB = new Date(b.dateOfLearning)
           return dateA - dateB //сортировка по возрастающей дате
         })
       }
-      document.querySelector('.table').textContent = ''
+      table.textContent = ''
       createTable(students)
-
-      document.querySelectorAll('.row').forEach((row, indexRow) => {
-        if (indexRow > 1) {
-        }
-      })
-
     })
   })
 }
@@ -154,23 +147,36 @@ document.querySelector('button').addEventListener('click', () => {
   const inputFaculty = document.querySelector('.faculty').value
   const inputDob = document.querySelector('.dob').value
   const inputDateOfLearning = +document.querySelector('.dateOfLearning').value
-
-  students.push({
-    name: inputName,
-    surname: inputSurname,
-    patronymic: inputPatronymic,
-    dob: new Date(inputDob),
-    dateOfLearning: inputDateOfLearning,
-    faculty: inputFaculty,
-  })
-
-  document.querySelector('.table').textContent = ''
-  createTable(students)
+  if(inputName && inputSurname && inputPatronymic && inputFaculty && inputDob && inputDateOfLearning) {
+    if(new Date(inputDob) > new Date('1900-01-01') && new Date(inputDob) < new Date()) {
+      if(inputDateOfLearning >= 2000 && inputDateOfLearning <= new Date().getFullYear()) {
+        students.push({
+          name: inputName,
+          surname: inputSurname,
+          patronymic: inputPatronymic,
+          dob: new Date(inputDob),
+          dateOfLearning: inputDateOfLearning,
+          faculty: inputFaculty,
+        })
+        table.textContent = ''
+        createTable(students)
+      } else alert('Некорректная дата начала обучения')
+    } else alert('Некорректная дата рождения')
+  } else alert('Все поля обязательны для заполнения')
 })
 
-const inputsSearch = document.querySelectorAll('.input-search')
-inputsSearch.forEach(el => {
+document.querySelectorAll('.input-search').forEach((el, index) => {
   el.oninput = () => {
-    console.log(123);
+    document.querySelectorAll('.row').forEach((row, indexRow) => {
+      if(indexRow > 1) {
+        row.remove()
+      }
+    })
+    students.map(student => {
+      const fullName =`${student.surname} ${student.name} ${student.patronymic}`
+      if(fullName.includes(el.value) || student.faculty.includes(el.value) || (`${student.dob.getDate()}.${student.dob.getMonth() + 1}.${student.dob.getFullYear()} (${new Date().getFullYear() - student.dob.getFullYear()})`).includes(el.value) || String(student.dateOfLearning).includes(el.value)) {
+        createRow(student)
+      }
+    })
   }
 })
